827be0e6be2afbad328c3f43dfdcef7bf7e66fd1
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-07 15:33:15 -0500

    Delete js directory

--- js/navbar.js
+++
@@ -1,4 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', "https://raw.githubusercontent.com/SirAlexBigBrain/GrassBandits/main/navbar.html", false);
-request.send();
-document.querySelector("body").innerHTML = request.responseText + document.querySelector("body").innerHTML;
