d578be50cf4d016b765cc0ba47e4646046c319fa
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-18 18:17:32 -0500

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -1,10 +1,15 @@
-var data = null;
+var data = [];
+var avatar = [];
+var avatarurl = "";
 var url = "https://api.github.com/repos/SirAlexBigBrain/GrassBandits/contributors";
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url);
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
-      data = JSON.parse(xhr.responseText);
-   }
+    data = JSON.parse(xhr.responseText);
+  }
 };
 xhr.send();
+data.forEach((element) => {
+  document.getElementById("foot").innerHTML += "<img src=\"" + "\">"
+});
